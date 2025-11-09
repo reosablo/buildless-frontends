5d8cf3296c4534c4e8a10759238558cfd4f1ab86
reosablo <reosablo@users.noreply.github.com>
2025-11-09 16:41:19 +0900

    refactor: rewrite with rxjs Observables

--- angular-17/main.ts
+++ angular-17/main.ts
@@ -1,12 +1,29 @@
 import "@angular/compiler";
 import "zone.js";
 
+import { AsyncPipe } from "@angular/common";
 import { HttpClient, provideHttpClient } from "@angular/common/http";
-import { Component, effect, inject, Injectable, signal } from "@angular/core";
-import { toSignal } from "@angular/core/rxjs-interop";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  Injectable,
+} from "@angular/core";
+import { FormControl, ReactiveFormsModule } from "@angular/forms";
 import { bootstrapApplication } from "@angular/platform-browser";
-import type { User } from "jsonplaceholder-types/types/user";
 import type { Post } from "jsonplaceholder-types/types/post";
+import type { User } from "jsonplaceholder-types/types/user";
+import {
+  BehaviorSubject,
+  distinctUntilChanged,
+  filter,
+  map,
+  type Observable,
+  share,
+  startWith,
+  switchMap,
+  tap,
+} from "rxjs";
 
 const urlBase = "https://jsonplaceholder.typicode.com";
 
@@ -32,25 +49,37 @@ class PostService {
 
 @Component({
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: "app-root",
+  imports: [AsyncPipe, ReactiveFormsModule],
   template: `
     <h1>Buildless Angular 17 app</h1>
-    <label>
-      Select User:
-      <select (change)="selectedUserId.set($event.target.value)">
-        <option hidden selected></option>
-        @for (user of users(); track user.id) {
-          <option value="{{ user.id }}">
-            &#64;{{ user.username }}: {{ user.name }}
-          </option>
+    @if (users$ | async; as users) {
+      <label>
+        Select User:
+        <select [formControl]="formControl">
+          <option hidden selected></option>
+          @for (user of users; track user.id) {
+            <option value="{{ user.id }}">
+              &#64;{{ user.username }}: {{ user.name }}
+            </option>
+          }
+        </select>
+      </label>
+    } @else if (loadingUsers$ | async) {
+      <p>Loading Users...</p>
+    }
+    @if (posts$ | async; as posts) {
+      <ul>
+        @for (post of posts; track post.id) {
+          <li>{{ post.title }}</li>
         }
-      </select>
-    </label>
-    <ul>
-      @for (post of posts(); track post.id) {
-        <li>{{ post.title }}</li>
-      }
-    </ul>
+      </ul>
+    } @else if (loadingPosts$ | async) {
+      <p>Loading Posts...</p>
+    } @else if (!(user$ | async)) {
+      <p>Select User to view posts</p>
+    }
     <p>
       Data Source:
       <a href="https://jsonplaceholder.typicode.com/" target="_blank">
@@ -60,22 +89,46 @@ class PostService {
   `,
 })
 class AppComponent {
-  readonly #postService = inject(PostService);
+  protected readonly formControl = new FormControl("");
+  protected readonly users$ = this.#getUsers();
+  protected readonly posts$ = this.#getPosts(this.#getSelectedUserId());
+  protected readonly loadingUsers$ = new BehaviorSubject(false);
+  protected readonly loadingPosts$ = new BehaviorSubject(false);
 
-  protected readonly users = toSignal(inject(UserService).getUsers());
-  protected readonly selectedUserId = signal<number | undefined>(undefined);
-  protected readonly posts = signal<unknown>(undefined);
+  #getSelectedUserId() {
+    return this.formControl.valueChanges.pipe(
+      startWith(this.formControl.value),
+      filter((id) => id !== null),
+      filter((id) => id !== ""),
+      map((id) => +id),
+      share(),
+    );
+  }
 
-  constructor() {
-    effect((onCleanup) => {
-      const selectedUserId = this.selectedUserId();
-      if (selectedUserId !== undefined) {
-        const subscription = this.#postService
-          .getPosts(selectedUserId)
-          .subscribe((posts) => this.posts.set(posts));
-        onCleanup(() => subscription.unsubscribe());
-      }
-    });
+  #getUsers() {
+    const userService = inject(UserService);
+    return userService.getUsers()
+      .pipe(
+        tap({
+          subscribe: () => this.loadingUsers$.next(true),
+          finalize: () => this.loadingUsers$.next(false),
+        }),
+        share(),
+      );
+  }
+
+  #getPosts(selectedUserId$: Observable<number>) {
+    const postService = inject(PostService);
+    return selectedUserId$.pipe(
+      distinctUntilChanged(),
+      switchMap((id) =>
+        postService.getPosts(id).pipe(tap({
+          subscribe: () => this.loadingPosts$.next(true),
+          finalize: () => this.loadingPosts$.next(false),
+        }))
+      ),
+      share(),
+    );
   }
 }
 
