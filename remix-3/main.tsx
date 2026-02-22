5ff6e1ac037e7611c474ae00e4eb3c2a99bd00ad
reosablo <reosablo@users.noreply.github.com>
2026-02-22 15:39:50 +0900

    fix(remix-3): update to remix@3.0.0-alpha.2

--- remix-3/main.tsx
+++ remix-3/main.tsx
@@ -1,11 +1,10 @@
-/** @jsxImportSource @remix-run/dom */
+/** @jsxImportSource remix/component */
 
-import { createRoot, type Remix } from "@remix-run/dom";
-import { dom } from "@remix-run/events";
+import { createRoot, type Handle } from "remix/component";
 import type { Post } from "jsonplaceholder-types/types/post";
 import type { User } from "jsonplaceholder-types/types/user";
 
-function App(this: Remix.Handle) {
+function App(handle: Handle) {
   const urlBase = "https://jsonplaceholder.typicode.com";
 
   let users: User[] | undefined;
@@ -15,39 +14,38 @@ function App(this: Remix.Handle) {
 
   const fetchUsers = async (signal: AbortSignal) => {
     loadingUsers = true;
-    this.update();
+    handle.update();
     try {
       const response = await fetch(`${urlBase}/users`, { signal });
-      const data = await response.json() as User[];
+      const data = (await response.json()) as User[];
       signal.throwIfAborted();
       users = data;
       loadingUsers = false;
-      this.update();
+      handle.update();
     } catch (error) {
       if (signal.aborted) return;
       loadingUsers = false;
-      this.update();
+      handle.update();
       throw error;
     }
   };
 
   const fetchPosts = async (userId: number, signal: AbortSignal) => {
     loadingPosts = true;
-    this.update();
+    handle.update();
     try {
-      const response = await fetch(
-        `${urlBase}/posts?userId=${userId}`,
-        { signal },
-      );
-      const data = await response.json() as Post[];
+      const response = await fetch(`${urlBase}/posts?userId=${userId}`, {
+        signal,
+      });
+      const data = (await response.json()) as Post[];
       signal.throwIfAborted();
       posts = data;
       loadingPosts = false;
-      this.update();
+      handle.update();
     } catch (error) {
       if (signal.aborted) return;
       loadingPosts = false;
-      this.update();
+      handle.update();
       throw error;
     }
   };
@@ -56,40 +54,40 @@ function App(this: Remix.Handle) {
     fetchPosts(userId, signal);
   };
 
-  this.queueTask(() => {
-    fetchUsers(this.signal);
+  handle.queueTask(() => {
+    fetchUsers(handle.signal);
   });
 
   return () => (
     <>
       <h1>Buildless Remix 3 app</h1>
-      {users !== undefined && (
-            <label>
-              Select User:
-              <select
-                on={[
-                  dom.change(function handleChange(event, signal) {
-                    selectUserId(+event.currentTarget.value, signal);
-                  }),
-                ]}
-              >
-                <option hidden selected></option>
-                {users.map((user) => (
-                  <option key={user.id} value={user.id}>
-                    @{user.username}: {user.name}
-                  </option>
-                ))}
-              </select>
-            </label>
-          ) ||
-        loadingUsers && <p>Loading Users...</p>}
-      {posts !== undefined && (
-            <ul>
-              {posts.map((post) => <li key={post.id}>{post.title}</li>)}
-            </ul>
-          ) ||
-        loadingPosts && <p>Loading Posts...</p> ||
-        users !== undefined && <p>Select User to view posts</p>}
+      {(users !== undefined && (
+        <label>
+          Select User:
+          <select
+            on={{
+              change(event, signal) {
+                selectUserId(+event.currentTarget.value, signal);
+              },
+            }}
+          >
+            <option hidden selected></option>
+            {users.map((user) => (
+              <option key={user.id} value={user.id}>
+                @{user.username}: {user.name}
+              </option>
+            ))}
+          </select>
+        </label>
+      )) ||
+        (loadingUsers && <p>Loading Users...</p>)}
+      {(posts !== undefined && (
+        <ul>
+          {posts.map((post) => <li key={post.id}>{post.title}</li>)}
+        </ul>
+      )) ||
+        (loadingPosts && <p>Loading Posts...</p>) ||
+        (users !== undefined && <p>Select User to view posts</p>)}
     </>
   );
 }
