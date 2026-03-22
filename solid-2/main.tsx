609215c4436bf80b460cfa429ffeeae61b39e8ab
reosablo <reosablo@users.noreply.github.com>
2026-03-22 17:25:15 +0900

    feat(solid-2): add source viewer

--- solid-2/main.tsx
+++ solid-2/main.tsx
@@ -1,102 +1,87 @@
 /** @jsxImportSource solid-js */
-// deno-lint-ignore-file no-import-prefix
 
 import { render } from "@solidjs/web";
-import type {
-  Post,
-  User,
-} from "https://esm.sh/*@untypeable/jsonplaceholder@1.0.2";
 import {
+  createEffect,
   createMemo,
   createSignal,
   For,
   isPending,
-  Loading,
-  Match,
   onCleanup,
-  Switch,
 } from "solid-js";
+import { fetchReadmeHTML, fetchSourceHTML } from "./utils.js";
 
-const urlBase = "https://jsonplaceholder.typicode.com";
+const sources = {
+  "./index.html": { lang: "html" },
+  "./main.tsx": { lang: "tsx" },
+  "./utils.js": { lang: "javascript" },
+} as const satisfies { [url: string]: { lang: string } };
 
-async function fetchUsers() {
-  const ctrl = new AbortController();
-  onCleanup(() => ctrl.abort());
-  const response = await fetch(`${urlBase}/users`, { signal: ctrl.signal });
-  return await response.json() as User[];
-}
-
-async function fetchPosts(userId: number) {
-  const ctrl = new AbortController();
-  onCleanup(() => ctrl.abort());
-  const response = await fetch(
-    `${urlBase}/posts?userId=${userId}`,
-    { signal: ctrl.signal },
-  );
-  return await response.json() as Post[];
-}
-
-async function fetchReadmeHTML() {
-  const ctrl = new AbortController();
-  onCleanup(() => ctrl.abort());
-  const [{ marked }, readmeMarkdown] = await Promise.all([
-    import("https://esm.sh/*marked@17.0.0"),
-    fetch("./README.md", { signal: ctrl.signal }).then((res) => res.text()),
-  ]);
-  return marked(readmeMarkdown);
-}
-
-function App() {
-  const [selectedUserId, setSelectedUserId] = createSignal<number>();
-  const users = createMemo(() => fetchUsers());
-  const posts = createMemo(() => {
-    const userId = selectedUserId();
-    return userId !== undefined ? fetchPosts(userId) : [];
+function SourceView(
+  props: {
+    url: keyof typeof sources;
+    onLoadingChange?: (loading: boolean) => void;
+  },
+) {
+  const html = createMemo(() => {
+    const ctrl = new AbortController();
+    onCleanup(() => ctrl.abort());
+    return fetchSourceHTML(props.url, sources[props.url].lang, ctrl.signal);
   });
-  const readmeHTML = createMemo(() => fetchReadmeHTML());
+
+  createEffect(
+    () => isPending(html),
+    (loading) => {
+      props.onLoadingChange?.(loading);
+    },
+  );
+
+  return <section innerHTML={html()}></section>;
+}
+
+function SourcesView() {
+  const [selectedSourceUrl, setSelectedSourceUrl] = createSignal<
+    keyof typeof sources
+  >("./index.html");
+  const [loading, setLoading] = createSignal(false);
 
   return (
     <>
-      <section innerHTML={readmeHTML()}></section>
-      <Loading fallback={<p>Loading Users...</p>}>
-        <label>
-          Select User:
-          <select
-            onChange={function handleChange(event) {
-              setSelectedUserId(+event.currentTarget.value);
-            }}
-          >
-            <option hidden selected></option>
-            <For each={users()}>
-              {(user) => (
-                <option value={user().id}>
-                  @{user().username}: {user().name}
-                </option>
-              )}
-            </For>
-          </select>
-        </label>
-      </Loading>
-      <Switch>
-        <Match when={selectedUserId() !== undefined}>
-          <Loading fallback={<p>Loading Posts...</p>}>
-            <ul>
-              <For each={posts()}>
-                {(post) => <li>{post().title}</li>}
-              </For>
-            </ul>
-          </Loading>
-        </Match>
-        <Match when={!isPending(users)}>
-          <p>Select User to view posts</p>
-        </Match>
-      </Switch>
-      <p>
-        Data Source:
-        <a href="https://jsonplaceholder.typicode.com/" target="_blank">
-          JSONPlaceholder
-        </a>
-      </p>
+      <label>
+        Source:
+        <select
+          onChange={function handleChange(event) {
+            setSelectedSourceUrl(
+              event.currentTarget.value as keyof typeof sources,
+            );
+          }}
+        >
+          <For each={Object.keys(sources)}>
+            {(url) => <option value={url()}>{url()}</option>}
+          </For>
+        </select>
+      </label>
+      {loading() && <progress />}
+      <SourceView url={selectedSourceUrl()} onLoadingChange={setLoading} />
+    </>
+  );
+}
+
+function ReadmeView() {
+  const readmeHTML = createMemo(() => {
+    const ctrl = new AbortController();
+    onCleanup(() => ctrl.abort());
+    return fetchReadmeHTML(ctrl.signal);
+  });
+
+  return <section innerHTML={readmeHTML()}></section>;
+}
+
+function App() {
+  return (
+    <>
+      <ReadmeView />
+      <SourcesView />
     </>
   );
 }
