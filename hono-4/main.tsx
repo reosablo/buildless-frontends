e21cd26b53ecd3f5e60b0a027a5afb5a0481479e
reosablo <reosablo@users.noreply.github.com>
2026-03-22 17:23:16 +0900

    feat(hono-4): add source viewer

--- hono-4/main.tsx
+++ hono-4/main.tsx
@@ -1,146 +1,106 @@
 /** @jsxImportSource hono/jsx */
-// deno-lint-ignore-file no-import-prefix
 
 import { useEffect, useState } from "hono/jsx";
 import { createRoot } from "hono/jsx/dom/client";
-import type {
-  Post,
-  User,
-} from "https://esm.sh/*@untypeable/jsonplaceholder@1.0.2";
+import { fetchReadmeHTML, fetchSourceHTML } from "./utils.js";
 
-const urlBase = "https://jsonplaceholder.typicode.com";
+const sources = {
+  "./index.html": { lang: "html" },
+  "./main.tsx": { lang: "tsx" },
+  "./utils.js": { lang: "javascript" },
+} as const satisfies { [url: string]: { lang: string } };
 
-function useUsers() {
-  const [users, setUsers] = useState<User[] | undefined>(undefined);
+function useSourceHTML(url: keyof typeof sources) {
+  const [sourceHTML, setSourceHTML] = useState<string | undefined>(undefined);
   const [loading, setLoading] = useState(false);
 
-  async function fetchUsers(signal: AbortSignal) {
-    setLoading(true);
-    try {
-      const response = await fetch(`${urlBase}/users`, { signal });
-      const users = await response.json() as User[];
-      signal.throwIfAborted();
-      setUsers(users);
-      setLoading(false);
-    } catch (error) {
-      if (signal.aborted) return;
-      setLoading(false);
-      throw error;
-    }
-  }
-
-  useEffect(function updateUsers() {
+  useEffect(function updateSourceHTML() {
     const ctrl = new AbortController();
-    fetchUsers(ctrl.signal);
-    return () => ctrl.abort();
-  }, []);
-
-  return [users, { loading }] as const;
-}
-
-function usePosts(userId: number | undefined) {
-  const [posts, setPosts] = useState<Post[] | undefined>(undefined);
-  const [loading, setLoading] = useState(false);
-
-  async function fetchPosts(userId: number, signal: AbortSignal) {
     setLoading(true);
-    try {
-      const response = await fetch(
-        `${urlBase}/posts?userId=${userId}`,
-        { signal },
-      );
-      const posts = await response.json() as Post[];
-      signal.throwIfAborted();
-      setPosts(posts);
-      setLoading(false);
-    } catch (error) {
-      if (signal.aborted) return;
-      setLoading(false);
-      throw error;
-    }
-  }
-
-  useEffect(function updatePosts() {
-    if (userId === undefined) return;
-    const ctrl = new AbortController();
-    fetchPosts(userId, ctrl.signal);
+    fetchSourceHTML(url, sources[url].lang, ctrl.signal)
+      .then(setSourceHTML, (error) => {
+        if (!ctrl.signal.aborted) throw error;
+      })
+      .finally(() => {
+        if (!ctrl.signal.aborted) setLoading(false);
+      });
     return () => ctrl.abort();
-  }, [userId]);
+  }, [url]);
 
-  return [posts, { loading }] as const;
+  return [sourceHTML, { loading }] as const;
 }
 
 function useReadmeHTML() {
   const [readmeHTML, setReadmeHTML] = useState<string | undefined>(undefined);
 
-  async function fetchReadmeHTML(signal: AbortSignal) {
-    try {
-      const [{ marked }, readmeMarkdown] = await Promise.all([
-        import("https://esm.sh/*marked@17.0.0"),
-        fetch("./README.md", { signal }).then((res) => res.text()),
-      ]);
-      const readmeHTML = await marked(readmeMarkdown);
-      signal.throwIfAborted();
-      setReadmeHTML(readmeHTML);
-    } catch (error) {
-      if (signal.aborted) return;
-      throw error;
-    }
-  }
-
-  useEffect(function updateReadme() {
+  useEffect(function updateReadmeHTML() {
     const ctrl = new AbortController();
-    fetchReadmeHTML(ctrl.signal);
+    fetchReadmeHTML(ctrl.signal)
+      .then(setReadmeHTML, (error) => {
+        if (!ctrl.signal.aborted) throw error;
+      });
     return () => ctrl.abort();
   }, []);
 
   return [readmeHTML] as const;
 }
 
-function App() {
-  const [selectedUserId, setSelectedUserId] = useState<number | undefined>(
-    undefined,
-  );
-  const [users, { loading: loadingUsers }] = useUsers();
-  const [posts, { loading: loadingPosts }] = usePosts(selectedUserId);
-  const [readmeHTML] = useReadmeHTML();
+function SourceView(props: {
+  url: keyof typeof sources;
+  onLoadingChange?: (loading: boolean) => void;
+}) {
+  const [sourceHTML, { loading }] = useSourceHTML(props.url);
+
+  useEffect(() => {
+    props.onLoadingChange?.(loading);
+  }, [loading, props.onLoadingChange]);
+
+  return <div dangerouslySetInnerHTML={{ __html: sourceHTML ?? "" }}></div>;
+}
+
+function SourcesView() {
+  const [selectedSourceUrl, setSelectedSourceUrl] = useState<
+    keyof typeof sources
+  >("./index.html");
+  const [loading, setLoading] = useState(false);
 
   return (
     <>
-      <section dangerouslySetInnerHTML={{ __html: readmeHTML ?? "" }}></section>
-      {users !== undefined && (
-            <label>
-              Select User:
-              <select
-                onChange={function handleChange(event) {
-                  setSelectedUserId(
-                    +(event.currentTarget as HTMLSelectElement)
-                      .value,
-                  );
-                }}
-              >
-                <option hidden selected></option>
-                {users.map((user) => (
-                  <option key={user.id} value={user.id}>
-                    @{user.username}: {user.name}
-                  </option>
-                ))}
-              </select>
-            </label>
-          ) || loadingUsers && <p>Loading Users...</p>}
-      {posts !== undefined && (
-            <ul>
-              {posts.map((post) => <li key={post.id}>{post.title}</li>)}
-            </ul>
-          ) ||
-        loadingPosts && <p>Loading Posts...</p> ||
-        users !== undefined && <p>Select User to view posts</p>}
-      <p>
-        Data Source:
-        <a href="https://jsonplaceholder.typicode.com/" target="_blank">
-          JSONPlaceholder
-        </a>
-      </p>
+      <label>
+        Source:
+        <select
+          onChange={function handleChange(event) {
+            setSelectedSourceUrl(
+              (event.currentTarget as HTMLSelectElement)
+                .value as keyof typeof sources,
+            );
+          }}
+        >
+          {Object.keys(sources).map((url) => (
+            <option key={url} value={url}>{url}</option>
+          ))}
+        </select>
+      </label>
+      {loading && <progress />}
+      <SourceView
+        url={selectedSourceUrl}
+        onLoadingChange={setLoading}
+      />
+    </>
+  );
+}
+
+function ReadmeView() {
+  const [readmeHTML] = useReadmeHTML();
+
+  return <div dangerouslySetInnerHTML={{ __html: readmeHTML ?? "" }}></div>;
+}
+
+function App() {
+  return (
+    <>
+      <ReadmeView />
+      <SourcesView />
     </>
   );
 }
